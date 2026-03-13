bb41246cfca5fa39aadc610dae86c1afabf784fa
Scott <booksearchers219@gmail.com>
2026-03-13 11:09:17 -0700

    better graph resizing

--- chart_viewer.py
+++ chart_viewer.py
@@ -4,37 +4,38 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 IMAGE_FILE = "bot_dashboard.png"
-REFRESH_SECONDS = 300   # 5 minutes
-
-
-def load_and_display():
-    if not os.path.exists(IMAGE_FILE):
-        print("Chart file not found:", IMAGE_FILE)
-        return None
-
-    img = mpimg.imread(IMAGE_FILE)
-
-    plt.clf()
-    plt.imshow(img)
-    plt.axis("off")
-    plt.title("Trading Chart (auto refresh every 5 minutes)")
-
-    plt.draw()
+REFRESH_SECONDS = 300  # 5 minutes
 
 
 def main():
 
-    plt.ion()  # interactive mode
-    plt.figure(figsize=(10, 6))
+    plt.ion()
+
+    fig, ax = plt.subplots()
+    ax.axis("off")
+
+    img = None
+    im_obj = None
 
     while True:
-        print("Refreshing chart...")
-        load_and_display()
-        plt.pause(1)
 
+        if os.path.exists(IMAGE_FILE):
+
+            img = mpimg.imread(IMAGE_FILE)
+
+            if im_obj is None:
+                im_obj = ax.imshow(img)
+            else:
+                im_obj.set_data(img)
+
+            fig.canvas.draw_idle()
+
+        else:
+            print("Waiting for chart.png...")
+
+        plt.pause(1)
         time.sleep(REFRESH_SECONDS)
 
 
 if __name__ == "__main__":
     main()
-
